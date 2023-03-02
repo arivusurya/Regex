630d2780fafd2fae2b410854b2ba87e45c33136f
Arivumathi R <60580194+arivusurya@users.noreply.github.com>
2023-03-02 12:24:36 +0530

    validate the first name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace UserValidation{
         public static void Main(string[] args)
         {
             User userinfo = new User();
-            Boolean Firstname = userinfo.ValidateFirstName("ari");
+            Boolean Firstname = userinfo.ValidateFirstName("Kratos");
            if(Firstname == false){
                Console.WriteLine("Enter the first name with capitalze and atleast had a 3 character");
            }
