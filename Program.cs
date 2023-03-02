0e551272dfbfd13276369062ded6c8c7937fa55c
arivusurya <arivusurya.3@gmail.com>
2023-03-02 12:25:37 +0530

    validate the last name

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace UserValidation{
         public static void Main(string[] args)
         {
             User userinfo = new User();
-            Boolean Firstname = userinfo.ValidateFirstName("ari");
+            Boolean Firstname = userinfo.ValidateName("Zeus");
            if(Firstname == false){
                Console.WriteLine("Enter the first name with capitalze and atleast had a 3 character");
            }
--- User.cs
+++ User.cs
@@ -6,7 +6,7 @@ namespace UserValidation{
 
     public class User{
 
-            public Boolean ValidateFirstName(string firstName){   
+            public Boolean ValidateName(string firstName){   
                String Pattern = "[A-Z]{1}[a-z]{2,}";
                return Regex.IsMatch(firstName,Pattern);
             }
